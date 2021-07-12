b88131709a0c29ee41b6d88af8fce2ce646170db
zhoujun <zhoujun@qq.com>
2021-07-12 15:12:16 +0800

    add trian models

--- training/convert_to_onnx.py
+++ training/convert_to_onnx.py
@@ -10,14 +10,14 @@ model_path = "./VGG_192x192_models/20.pth"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 checkpoint = torch.load(model_path, map_location=device)
-net = PFLDInference()
+net = VGG_192x192()
 net.load_state_dict(checkpoint)
 net.eval()
 net.to(device)
 
 model_name = model_path.split("/")[-1].split(".")[0]
-model_path = f"models/checkpoint/0_25/list25p_gray_rgb_shake3w_shake17w_shake7w/{model_name}.onnx"
+model_path = f"./VGG_192x192_models/20.onnx"
 
-dummy_input = torch.randn(1, 3, 112, 112).to(device)
+dummy_input = torch.randn(1, 3, 192, 192).to(device)
 
-torch.onnx.export(net, dummy_input, model_path, export_params=True, verbose=False, input_names=['input'], output_names=['pose', 'landms'])
+torch.onnx.export(net, dummy_input, model_path, export_params=True, verbose=False, input_names=['input'], output_names=['out'], opset_version=11)
--- training/train_VGG_192x192.py
+++ training/train_VGG_192x192.py
@@ -20,7 +20,7 @@ LR = 0.0001
 EPOCH = 200
 save_model_path = "./VGG_192x192_models/"
 
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(torch.cuda.is_available())
 normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 transform = transforms.Compose([
@@ -30,15 +30,16 @@ transform = transforms.Compose([
     transforms.ToTensor(),  # 将图片转换为Tensor,归一化至[0,1]
     normalize
 ])
-#train_dataset = torchvision.datasets.ImageFolder(root='\\\\10.1.1.63\\i\\DMS\\DOAData\\train', transform=transform)
-train_dataset = torchvision.datasets.ImageFolder(root='../210w/train', transform=transform)
+train_dataset = torchvision.datasets.ImageFolder(root=r'\\10.1.1.125\Development\ShareFolder\dog_cat_data\train', transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BARCH_SIZE, shuffle=True)
 
-#validation_dataset = torchvision.datasets.ImageFolder(root='\\\\10.1.1.63\\i\\DMS\\DOAData\\test', transform=transform)
-validation_dataset = torchvision.datasets.ImageFolder(root='../210w/test', transform=transform)
+validation_dataset = torchvision.datasets.ImageFolder(root=r'\\10.1.1.125\Development\ShareFolder\dog_cat_data\test', transform=transform)
 test_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=32, shuffle=True)
 
 VGGNet = VGG_192x192(2).to(device)
+pthfile = r'./VGG_192x192_models/180.pth'
+VGGNet.load_state_dict(torch.load(pthfile))
+
 #alexNet = alexNet(pretrained=True)
 #alexNet = torch.load("./alexNet.pth")
 criterion = nn.CrossEntropyLoss()
@@ -66,8 +67,8 @@ if __name__ == '__main__':
             loss.backward()
             opti.step()
             sum_loss += loss.item()
-            if i % 100 == 0:
-                print("EPOCH:", epoch, " Iteration :", i," Ave loss:", sum_loss / 100, " lr:",LR)
+            if i % 50 == 0:
+                print("EPOCH:", epoch, " Iteration :", i," Ave loss:", sum_loss / 50, " lr:",LR)
                 sum_loss = 0.0
         Accuracy_list.append(100.0 * correct1 / total1)
         print('train accurary={}'.format(100.0 * correct1 / total1))
